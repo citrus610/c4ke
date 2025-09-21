b68a6554bb3ad3313b3f0452a53b63930bf317b2
cj5716 <125858804+cj5716@users.noreply.github.com>
2025-09-21 20:06:49 +0800

    remove extraneous brackets
    size: 3981
    bench: 4811098

--- src/board.cpp
+++ src/board.cpp
@@ -29,8 +29,8 @@ struct Board {
         if (piece < PIECE_NONE) {
             hash ^= KEYS[piece][square];
 
-            pieces[piece / 2] |= (1ull << square);
-            colors[piece & 1] |= (1ull << square);
+            pieces[piece / 2] |= 1ull << square;
+            colors[piece & 1] |= 1ull << square;
 
             if (piece / 2 < KNIGHT)
                 hash_pawn ^= KEYS[piece][square];
