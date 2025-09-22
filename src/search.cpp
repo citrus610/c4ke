904084c43bc6469bb90642164d37a8d23bda7bdf
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-09-22 18:57:00 +0000

    only print uci info in main thread for mini build
    size: 3976
    bench: 4811098

--- src/search.cpp
+++ src/search.cpp
@@ -173,7 +173,7 @@ struct Thread {
                 depth_next = depth - 1,
                 score;
 
-            // Quiet pruning and SEE pruning in qsearch
+            // Quiet pruning, SEE pruning and LMP in qsearch
             if (!depth && best > -WIN && move_scores[i] < 1e6)
                 break;
 
@@ -359,12 +359,12 @@ struct Thread {
             // Print info
 #ifdef OB_MINI
             if (!BENCH && !id) {
+#else
+            if (!id) {
 #endif
                 cout << "info depth " << depth << " score cp " << score << " pv ";
                 move_print(pv);
-#ifdef OB_MINI
             }
-#endif
 
             // Check time
             if (now() > LIMIT_SOFT && !id)
