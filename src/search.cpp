b917e43372e152fa76264fb2cd04c3753f6ed844
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-11-02 02:01:55 +0700

    no futility pruning
    size: 4003
    bench: 4718290

--- src/search.cpp
+++ src/search.cpp
@@ -189,10 +189,6 @@ struct Thread {
             if (!is_pv && !board.checkers && quiet_count > 1 + depth * depth >> !is_improving && is_quiet)
                 continue;
 
-            // Futility pruning
-            if (ply && best > -WIN && depth < 10 && !board.checkers && stack_eval[ply] + 96 * depth + move_scores[i] / 32 + 100 < alpha && is_quiet)
-                continue;
-
             // SEE pruning in pvsearch
             if (ply && best > -WIN && move_scores[i] < 1e6 && !board.see(move, -81 * depth))
                 continue;
