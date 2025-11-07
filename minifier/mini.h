f78e55852f7aa4b1b13f5a044c3deb1967a5c7f1
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-11-08 00:14:38 +0700

    misc: update comments
    size: 4061
    bench: 4433439

--- minifier/mini.h
+++ minifier/mini.h
@@ -661,7 +661,7 @@ inline std::vector<Constant> get_constants(std::string str, bool is_tcec)
         }
 
         // Replace values for TCEC build
-        // Use 512 threads
+        // Use 256 threads
         if (is_tcec && name == "THREADS") {
             value = "256";
         }
