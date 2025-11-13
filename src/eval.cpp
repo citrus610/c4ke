ca246ea8bbe04a3adfe4c833e46502f6d07e7bbd
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-11-14 00:47:47 +0700

    king threat no queen
    size: 4095
    bench: 4617513

--- src/eval.cpp
+++ src/eval.cpp
@@ -5,24 +5,24 @@
 i32 PHASE[] { 0, 1, 1, 2, 4, 0 },
     VALUE[] { 110, 319, 294, 518, 912, 2000, 0 },
     LAYOUT[] { ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK },
-    MATERIAL[] { S(95, 196), S(362, 575), S(378, 583), S(504, 1050), S(1294, 1716), 0 };
+    MATERIAL[] { S(95, 206), S(360, 586), S(375, 595), S(506, 1062), S(1271, 1795), 0 };
 
 #define SCALE 8
 
-#define BISHOP_PAIR S(29, 95)
-#define KING_OPEN S(-69, -6)
-#define KING_SEMIOPEN S(-33, 12)
-#define KING_PAWN_THREAT S(19, 50)
+#define BISHOP_PAIR S(30, 96)
+#define KING_OPEN S(-69, -7)
+#define KING_SEMIOPEN S(-33, 13)
+#define KING_PAWN_THREAT S(14, 53)
 #define ROOK_OPEN S(25, -5)
-#define ROOK_SEMIOPEN S(16, 17)
-#define PAWN_PROTECTED S(23, 27)
+#define ROOK_SEMIOPEN S(15, 19)
+#define PAWN_PROTECTED S(23, 28)
 #define PAWN_DOUBLED S(10, 39)
-#define PAWN_SHIELD S(29, -14)
-#define PASSER_BLOCKED S(-7, 66)
+#define PAWN_SHIELD S(29, -13)
+#define PASSER_BLOCKED S(-10, 72)
 
 #define TEMPO 20
 
-#define DATA_STR "0-,-/570,/13684 /12233/'1//.120/121001.--5+$&343$&&()))%\"%'))*('%'('(')'%%&()()&%%&''(*)(,' $!+)21,* %$#% & !#'32  #!** ' ,7' !%$'(+' ,*(''$$&$\"#$)6&\"$'*+'$#$%'''&&'#\"#&''*)  %(***&\"$')))) ))&%&'(&%')))(&#&''((('%(()'&'&&%&())(&% %)+**& (*)/  $,4?? \"$+BA+/( <0J '* A-1*'$! \"' $(,.1/"
+#define DATA_STR "0-,-/670,/13684 /12233/'0//.1200121001.--5+#&343%&&()))%\"%')))('%'('(')'%%&()()&%%%&'(*)(,' $!+)21,+ '&%' ( !#&32  #!)) % (E* #'&)*.* 1,*)(%$'$\"#$)6'#&(+,(%#%&(((''($$$'((+* !&)+++&#%(****!))&%&'(&$')))(&#&''((('%(()'&'&&%&))*(&% %)+**& ')(,  $,4@@ \"$,DD*.& 9)C OZ N/2+($\" !) %)-032"
 
 #define INDEX_EG 141
 
@@ -37,14 +37,14 @@ i32 PHASE[] { 0, 1, 1, 2, 4, 0 },
 #define INDEX_KING_PASSER_US 125
 #define INDEX_KING_PASSER_THEM 133
 
-#define OFFSET_MOBILITY S(-9, -3)
-#define OFFSET_PASSER S(-3, -3)
+#define OFFSET_MOBILITY S(-9, -2)
+#define OFFSET_PASSER S(-5, -3)
 #define OFFSET_PHALANX S(1, 1)
-#define OFFSET_THREAT S(10, -8)
-#define OFFSET_PUSH_THREAT S(19, -22)
-#define OFFSET_KING_ATTACK S(11, -14)
-#define OFFSET_PST S(-23, -13)
-#define OFFSET_KING_PASSER S(-17, -15)
+#define OFFSET_THREAT S(10, -7)
+#define OFFSET_PUSH_THREAT S(20, -16)
+#define OFFSET_KING_ATTACK S(8, -27)
+#define OFFSET_PST S(-23, -14)
+#define OFFSET_KING_PASSER S(-21, -16)
 
 i32 get_data(i32 index) {
     auto data = DATA_STR;
