7f86719c8e2400c6cec85fd8f23f8b60ff5f28a3
citrus610 <79373642+citrus610@users.noreply.github.com>
2025-11-17 23:58:00 +0700

    misc: remove redundant tests
    size: 4085
    bench: 5431457

--- src/main.cpp
+++ src/main.cpp
@@ -44,258 +44,6 @@ u64 perft(Board& board, i32 depth, bool is_root = FALSE) {
 
     return nodes;
 }
-
-void test_perft() {
-    struct Test {
-        string fen;
-        i32 depth;
-        u64 result;
-    };
-
-    std::vector<Test> tests = {
-        Test { "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6, 119060324 },
-        Test { "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5, 193690690 },
-        Test { "4k3/8/8/8/8/8/8/4K2R w K - 0 1", 6, 764643 },
-        Test { "4k3/8/8/8/8/8/8/R3K3 w Q - 0 1", 6, 846648 },
-        Test { "4k2r/8/8/8/8/8/8/4K3 w k - 0 1", 6, 899442 },
-        Test { "r3k3/8/8/8/8/8/8/4K3 w q - 0 1", 6, 1001523 },
-        Test { "4k3/8/8/8/8/8/8/R3K2R w KQ - 0 1", 6, 2788982 },
-        Test { "r3k2r/8/8/8/8/8/8/4K3 w kq - 0 1", 6, 3517770 },
-        Test { "8/8/8/8/8/8/6k1/4K2R w K - 0 1", 6, 185867 },
-        Test { "8/8/8/8/8/8/1k6/R3K3 w Q - 0 1", 6, 413018 },
-        Test { "4k2r/6K1/8/8/8/8/8/8 w k - 0 1", 6, 179869 },
-        Test { "r3k3/1K6/8/8/8/8/8/8 w q - 0 1", 6, 367724 },
-        Test { "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1", 6, 179862938 },
-        Test { "r3k2r/8/8/8/8/8/8/1R2K2R w Kkq - 0 1", 6, 195629489 },
-        Test { "r3k2r/8/8/8/8/8/8/2R1K2R w Kkq - 0 1", 6, 184411439 },
-        Test { "r3k2r/8/8/8/8/8/8/R3K1R1 w Qkq - 0 1", 6, 189224276 },
-        Test { "1r2k2r/8/8/8/8/8/8/R3K2R w KQk - 0 1", 6, 198328929 },
-        Test { "2r1k2r/8/8/8/8/8/8/R3K2R w KQk - 0 1", 6, 185959088 },
-        Test { "r3k1r1/8/8/8/8/8/8/R3K2R w KQq - 0 1", 6, 190755813 },
-        Test { "4k3/8/8/8/8/8/8/4K2R b K - 0 1", 6, 899442 },
-        Test { "4k3/8/8/8/8/8/8/R3K3 b Q - 0 1", 6, 1001523 },
-        Test { "4k2r/8/8/8/8/8/8/4K3 b k - 0 1", 6, 764643 },
-        Test { "r3k3/8/8/8/8/8/8/4K3 b q - 0 1", 6, 846648 },
-        Test { "4k3/8/8/8/8/8/8/R3K2R b KQ - 0 1", 6, 3517770 },
-        Test { "r3k2r/8/8/8/8/8/8/4K3 b kq - 0 1", 6, 2788982 },
-        Test { "8/8/8/8/8/8/6k1/4K2R b K - 0 1", 6, 179869 },
-        Test { "8/8/8/8/8/8/1k6/R3K3 b Q - 0 1", 6, 367724 },
-        Test { "4k2r/6K1/8/8/8/8/8/8 b k - 0 1", 6, 185867 },
-        Test { "r3k3/1K6/8/8/8/8/8/8 b q - 0 1", 6, 413018 },
-        Test { "r3k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1", 6, 179862938 },
-        Test { "r3k2r/8/8/8/8/8/8/1R2K2R b Kkq - 0 1", 6, 198328929 },
-        Test { "r3k2r/8/8/8/8/8/8/2R1K2R b Kkq - 0 1", 6, 185959088 },
-        Test { "r3k2r/8/8/8/8/8/8/R3K1R1 b Qkq - 0 1", 6, 190755813 },
-        Test { "1r2k2r/8/8/8/8/8/8/R3K2R b KQk - 0 1", 6, 195629489 },
-        Test { "2r1k2r/8/8/8/8/8/8/R3K2R b KQk - 0 1", 6, 184411439 },
-        Test { "r3k1r1/8/8/8/8/8/8/R3K2R b KQq - 0 1", 6, 189224276 },
-        Test { "8/1n4N1/2k5/8/8/5K2/1N4n1/8 w - - 0 1", 6, 8107539 },
-        Test { "8/1k6/8/5N2/8/4n3/8/2K5 w - - 0 1", 6, 2594412 },
-        Test { "8/8/4k3/3Nn3/3nN3/4K3/8/8 w - - 0 1", 6, 19870403 },
-        Test { "K7/8/2n5/1n6/8/8/8/k6N w - - 0 1", 6, 588695 },
-        Test { "k7/8/2N5/1N6/8/8/8/K6n w - - 0 1", 6, 688780 },
-        Test { "8/1n4N1/2k5/8/8/5K2/1N4n1/8 b - - 0 1", 6, 8503277 },
-        Test { "8/1k6/8/5N2/8/4n3/8/2K5 b - - 0 1", 6, 3147566 },
-        Test { "8/8/3K4/3Nn3/3nN3/4k3/8/8 b - - 0 1", 6, 4405103 },
-        Test { "K7/8/2n5/1n6/8/8/8/k6N b - - 0 1", 6, 688780 },
-        Test { "k7/8/2N5/1N6/8/8/8/K6n b - - 0 1", 6, 588695 },
-        Test { "B6b/8/8/8/2K5/4k3/8/b6B w - - 0 1", 6, 22823890 },
-        Test { "8/8/1B6/7b/7k/8/2B1b3/7K w - - 0 1", 6, 28861171 },
-        Test { "k7/B7/1B6/1B6/8/8/8/K6b w - - 0 1", 6, 7881673 },
-        Test { "K7/b7/1b6/1b6/8/8/8/k6B w - - 0 1", 6, 7382896 },
-        Test { "B6b/8/8/8/2K5/5k2/8/b6B b - - 0 1", 6, 9250746 },
-        Test { "8/8/1B6/7b/7k/8/2B1b3/7K b - - 0 1", 6, 29027891 },
-        Test { "k7/B7/1B6/1B6/8/8/8/K6b b - - 0 1", 6, 7382896 },
-        Test { "K7/b7/1b6/1b6/8/8/8/k6B b - - 0 1", 6, 7881673 },
-        Test { "7k/RR6/8/8/8/8/rr6/7K w - - 0 1", 6, 44956585 },
-        Test { "R6r/8/8/2K5/5k2/8/8/r6R w - - 0 1", 6, 525169084 },
-        Test { "7k/RR6/8/8/8/8/rr6/7K b - - 0 1", 6, 44956585 },
-        Test { "R6r/8/8/2K5/5k2/8/8/r6R b - - 0 1", 6, 524966748 },
-        Test { "6kq/8/8/8/8/8/8/7K w - - 0 1", 6, 391507 },
-        Test { "6KQ/8/8/8/8/8/8/7k b - - 0 1", 6, 391507 },
-        Test { "K7/8/8/3Q4/4q3/8/8/7k w - - 0 1", 6, 3370175 },
-        Test { "6qk/8/8/8/8/8/8/7K b - - 0 1", 6, 419369 },
-        Test { "6KQ/8/8/8/8/8/8/7k b - - 0 1", 6, 391507 },
-        Test { "K7/8/8/3Q4/4q3/8/8/7k b - - 0 1", 6, 3370175 },
-        Test { "8/8/8/8/8/K7/P7/k7 w - - 0 1", 6, 6249 },
-        Test { "8/8/8/8/8/7K/7P/7k w - - 0 1", 6, 6249 },
-        Test { "K7/p7/k7/8/8/8/8/8 w - - 0 1", 6, 2343 },
-        Test { "7K/7p/7k/8/8/8/8/8 w - - 0 1", 6, 2343 },
-        Test { "8/2k1p3/3pP3/3P2K1/8/8/8/8 w - - 0 1", 6, 34834 },
-        Test { "8/8/8/8/8/K7/P7/k7 b - - 0 1", 6, 2343 },
-        Test { "8/8/8/8/8/7K/7P/7k b - - 0 1", 6, 2343 },
-        Test { "K7/p7/k7/8/8/8/8/8 b - - 0 1", 6, 6249 },
-        Test { "7K/7p/7k/8/8/8/8/8 b - - 0 1", 6, 6249 },
-        Test { "8/2k1p3/3pP3/3P2K1/8/8/8/8 b - - 0 1", 6, 34822 },
-        Test { "8/8/8/8/8/4k3/4P3/4K3 w - - 0 1", 6, 11848 },
-        Test { "4k3/4p3/4K3/8/8/8/8/8 b - - 0 1", 6, 11848 },
-        Test { "8/8/7k/7p/7P/7K/8/8 w - - 0 1", 6, 10724 },
-        Test { "8/8/k7/p7/P7/K7/8/8 w - - 0 1", 6, 10724 },
-        Test { "8/8/3k4/3p4/3P4/3K4/8/8 w - - 0 1", 6, 53138 },
-        Test { "8/3k4/3p4/8/3P4/3K4/8/8 w - - 0 1", 6, 157093 },
-        Test { "8/8/3k4/3p4/8/3P4/3K4/8 w - - 0 1", 6, 158065 },
-        Test { "k7/8/3p4/8/3P4/8/8/7K w - - 0 1", 6, 20960 },
-        Test { "8/8/7k/7p/7P/7K/8/8 b - - 0 1", 6, 10724 },
-        Test { "8/8/k7/p7/P7/K7/8/8 b - - 0 1", 6, 10724 },
-        Test { "8/8/3k4/3p4/3P4/3K4/8/8 b - - 0 1", 6, 53138 },
-        Test { "8/3k4/3p4/8/3P4/3K4/8/8 b - - 0 1", 6, 158065 },
-        Test { "8/8/3k4/3p4/8/3P4/3K4/8 b - - 0 1", 6, 157093 },
-        Test { "k7/8/3p4/8/3P4/8/8/7K b - - 0 1", 6, 21104 },
-        Test { "7k/3p4/8/8/3P4/8/8/K7 w - - 0 1", 6, 32191 },
-        Test { "7k/8/8/3p4/8/8/3P4/K7 w - - 0 1", 6, 30980 },
-        Test { "k7/8/8/7p/6P1/8/8/K7 w - - 0 1", 6, 41874 },
-        Test { "k7/8/7p/8/8/6P1/8/K7 w - - 0 1", 6, 29679 },
-        Test { "k7/8/8/6p1/7P/8/8/K7 w - - 0 1", 6, 41874 },
-        Test { "k7/8/6p1/8/8/7P/8/K7 w - - 0 1", 6, 29679 },
-        Test { "k7/8/8/3p4/4p3/8/8/7K w - - 0 1", 6, 22886 },
-        Test { "k7/8/3p4/8/8/4P3/8/7K w - - 0 1", 6, 28662 },
-        Test { "7k/3p4/8/8/3P4/8/8/K7 b - - 0 1", 6, 32167 },
-        Test { "7k/8/8/3p4/8/8/3P4/K7 b - - 0 1", 6, 30749 },
-        Test { "k7/8/8/7p/6P1/8/8/K7 b - - 0 1", 6, 41874 },
-        Test { "k7/8/7p/8/8/6P1/8/K7 b - - 0 1", 6, 29679 },
-        Test { "k7/8/8/6p1/7P/8/8/K7 b - - 0 1", 6, 41874 },
-        Test { "k7/8/6p1/8/8/7P/8/K7 b - - 0 1", 6, 29679 },
-        Test { "k7/8/8/3p4/4p3/8/8/7K b - - 0 1", 6, 22579 },
-        Test { "k7/8/3p4/8/8/4P3/8/7K b - - 0 1", 6, 28662 },
-        Test { "7k/8/8/p7/1P6/8/8/7K w - - 0 1", 6, 41874 },
-        Test { "7k/8/p7/8/8/1P6/8/7K w - - 0 1", 6, 29679 },
-        Test { "7k/8/8/1p6/P7/8/8/7K w - - 0 1", 6, 41874 },
-        Test { "7k/8/1p6/8/8/P7/8/7K w - - 0 1", 6, 29679 },
-        Test { "k7/7p/8/8/8/8/6P1/K7 w - - 0 1", 6, 55338 },
-        Test { "k7/6p1/8/8/8/8/7P/K7 w - - 0 1", 6, 55338 },
-        Test { "3k4/3pp3/8/8/8/8/3PP3/3K4 w - - 0 1", 6, 199002 },
-        Test { "7k/8/8/p7/1P6/8/8/7K b - - 0 1", 6, 41874 },
-        Test { "7k/8/p7/8/8/1P6/8/7K b - - 0 1", 6, 29679 },
-        Test { "7k/8/8/1p6/P7/8/8/7K b - - 0 1", 6, 41874 },
-        Test { "7k/8/1p6/8/8/P7/8/7K b - - 0 1", 6, 29679 },
-        Test { "k7/7p/8/8/8/8/6P1/K7 b - - 0 1", 6, 55338 },
-        Test { "k7/6p1/8/8/8/8/7P/K7 b - - 0 1", 6, 55338 },
-        Test { "3k4/3pp3/8/8/8/8/3PP3/3K4 b - - 0 1", 6, 199002 },
-        Test { "8/Pk6/8/8/8/8/6Kp/8 w - - 0 1", 6, 1030499 },
-        Test { "n1n5/1Pk5/8/8/8/8/5Kp1/5N1N w - - 0 1", 6, 37665329 },
-        Test { "8/PPPk4/8/8/8/8/4Kppp/8 w - - 0 1", 6, 28859283 },
-        Test { "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N w - - 0 1", 6, 71179139 },
-        Test { "8/Pk6/8/8/8/8/6Kp/8 b - - 0 1", 6, 1030499 },
-        Test { "n1n5/1Pk5/8/8/8/8/5Kp1/5N1N b - - 0 1", 6, 37665329 },
-        Test { "8/PPPk4/8/8/8/8/4Kppp/8 b - - 0 1", 6, 28859283 },
-        Test { "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N b - - 0 1", 6, 71179139 },
-        Test { "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 6, 11030083 },
-        Test { "rnbqkb1r/ppppp1pp/7n/4Pp2/8/8/PPPP1PPP/RNBQKBNR w KQkq f6 0 3", 5, 11139762 }
-    };
-
-    i32 passed = 0;
-
-    for (auto& test : tests) {
-        stringstream fen(test.fen);
-
-        Board board;
-        board.from_fen(fen);
-
-        u64 result = perft(board, test.depth);
-
-        if (result != test.result) {
-            cout << "FAILED: \n";
-            cout << "- fen: " << test.fen << "\n";
-            cout << "- depth: " << test.depth << "\n";
-            cout << "- expect: " << test.result << "\n";
-            cout << "- result: " << result << "\n";
-
-            break;
-        }
-        else {
-            passed += 1;
-
-            cout << "PASSED: " << passed << "/" << tests.size() << "\n";
-        }
-    }
-}
-
-void test_see()
-{
-    struct Test {
-        std::string fen;
-        i16 move;
-        i32 threshold;
-        bool result;
-    };
-
-    std::vector<Test> set = {
-        Test {
-            .fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-            .move = move_make(A1, B2),
-            .threshold = VALUE[PAWN],
-            .result = true
-        },
-        Test {
-            .fen = "k6b/8/8/8/8/2p5/1p6/BK6 w - - 0 1",
-            .move = move_make(A1, B2),
-            .threshold = VALUE[PAWN],
-            .result = false
-        },
-        Test {
-            .fen = "k7/8/8/8/8/2p5/1p6/BK6 w - - 0 1",
-            .move = move_make(A1, B2),
-            .threshold = VALUE[PAWN],
-            .result = false
-        },
-        Test {
-            .fen = "k7/8/8/8/8/2q5/1p6/BK6 w - - 0 1",
-            .move = move_make(A1, B2),
-            .threshold = VALUE[PAWN],
-            .result = true
-        },
-        Test {
-            .fen = "k6b/8/8/8/8/2q5/1p6/BK6 w - - 0 1",
-            .move = move_make(A1, B2),
-            .threshold = VALUE[PAWN],
-            .result = false
-        },
-        Test {
-            .fen = "rn2k2r/p3bpp1/2p4p/8/2P3Q1/1P1q4/P4P1P/RNB1K2R w KQkq - 0 8",
-            .move = move_make(G4, G7),
-            .threshold = 0,
-            .result = true
-        },
-        Test {
-            .fen = "r1bq1rk1/pppp1Npp/2nb1n2/4p3/2B1P3/2P5/PP1P1PPP/RNBQK2R b KQ - 0 6",
-            .move = move_make(F8, F7),
-            .threshold = 0,
-            .result = true
-        },
-        Test {
-            .fen = "r1bqkb1r/ppp1pppp/2n2n2/8/2BPP3/5P2/PP4PP/RNBQK1NR b KQkq - 0 5",
-            .move = move_make(C6, D4),
-            .threshold = 0,
-            .result = true
-        },
-        Test {
-            .fen = "5k2/1b6/8/3B4/4K3/8/8/8 w - - 0 1",
-            .move = move_make(D5, B7),
-            .threshold = 0,
-            .result = true
-        }
-    };
-
-    for (const auto& test : set) {
-        stringstream fen(test.fen);
-
-        Board board;
-        board.from_fen(fen);
-
-        bool see = board.see(test.move, test.threshold);
-
-        std::cout << std::endl;
-        std::cout << test.fen << std::endl;
-        std::cout << move_str(test.move) << std::endl;
-        
-        if (see == test.result) {
-            std::cout << "passed!" << std::endl;
-        }
-        else {
-            std::cout << "failed!" << std::endl;
-        }
-    }
-};
 #endif
 
 #ifdef OB_MINI
@@ -387,25 +135,14 @@ i32 main() {
 #ifdef OB
     // Perft
     if (argc > 2 && std::string(argv[1]) == "perft") {
-        if (std::string(argv[2]) == "test") {
-            test_perft();
-        }
-        else {
-            board.startpos();
+        board.startpos();
 
-            u64 time_1 = now();
-            u64 nodes = perft(board, std::stoi(argv[2]), true);
-            u64 time_2 = now();
+        u64 time_1 = now();
+        u64 nodes = perft(board, std::stoi(argv[2]), true);
+        u64 time_2 = now();
 
-            cout << nodes << " nodes " << (nodes * 1000 / max(time_2 - time_1, u64(1))) << " nps" << endl;
-        }
+        cout << nodes << " nodes " << (nodes * 1000 / max(time_2 - time_1, u64(1))) << " nps" << endl;
 
-        return 0;
-    }
-
-    // SEE test
-    if (argc > 1 && std::string(argv[1]) == "see") {
-        test_see();
         return 0;
     }
 #endif
@@ -415,7 +152,7 @@ i32 main() {
     cin >> token;
 
 #ifdef OB
-    cout << "id name c4ke v1.1" << endl;
+    cout << "id name c4ke v2.0" << endl;
     cout << "id author citrus610 and cj5716" << endl;
     cout << "option name Hash type spin default 8 min 1 max 67108864" << endl;
     cout << "option name Threads type spin default 1 min 1 max 2048" << endl;
